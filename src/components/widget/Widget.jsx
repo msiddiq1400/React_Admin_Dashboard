3285fa7a7233c904cd22da4b65d3c3de7783b1b3
siddiq <msiddiq1400@gmail.com>
2023-02-11 17:19:47 +0500

    added charts on home screen

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -10,16 +10,17 @@ const Widget = ({type}) => {
 
   switch(type) {
     case "user":
-        data = {title: "USERS", isMoney: false, link:"See all users", icon: <PersonOutline className='icon'/>}
+        data = {
+            title: "USERS", isMoney: false, link:"See all users", icon: <PersonOutline className='icon' style={{color: 'crimson', backgroundColor: 'rgba(255,0,0,0.2)'}}/>}
         break;
     case "order":
-        data = {title: "ORDERS", isMoney: false, link:"View all Orders", icon: <ShoppingCartOutlined className='icon'/>}
+        data = {title: "ORDERS", isMoney: false, link:"View all Orders", icon: <ShoppingCartOutlined className='icon' style={{color: 'goldenrod', backgroundColor: 'rgba(218,165,32,0.2)'}}/>}
         break;
     case "earning":
-        data = {title: "EARNINGS", isMoney: true, link:"View net earnings", icon: <MonetizationOnOutlined className='icon'/>}
+        data = {title: "EARNINGS", isMoney: true, link:"View net earnings", icon: <MonetizationOnOutlined className='icon' style={{color: 'green', backgroundColor: 'rgba(0,128,0,0.2)'}}/>}
         break;
     case "balance":
-        data = {title: "BALANCE", isMoney: true, link:"See Details", icon: <AccountBalanceWalletOutlined className='icon'/>}
+        data = {title: "BALANCE", isMoney: true, link:"See Details", icon: <AccountBalanceWalletOutlined className='icon' style={{color: 'purple', backgroundColor: 'rgba(128,0,128,0.2)'}}/>}
         break;
     default:
         break;
