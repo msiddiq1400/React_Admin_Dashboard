3229b72e13f9aeedb0daacbedbbf98ad48e6ca9e
siddiq <msiddiq1400@gmail.com>
2023-02-11 12:47:33 +0500

    manage sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,19 @@ import './sidebar.scss';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        Sidebar
+        <div className='top'>
+            <span className='logo'>Admin Panel</span>
+        </div>
+        <hr/>
+        <div className='center'>
+            <ul>
+                <li><span>Dashboard</span></li>
+                <li><span>Dashboard</span></li>
+                <li><span>Dashboard</span></li>
+                <li><span>Dashboard</span></li>
+            </ul>
+        </div>
+        <div className='bottom'>color options</div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-        Home
+        <Sidebar />
+        <div className="homeContainer">
+            container
+        </div>
     </div>
     
   )
