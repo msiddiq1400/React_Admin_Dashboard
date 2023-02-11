78e8baf87d9dffa32c1e930244b107c3aebf0327
siddiq <msiddiq1400@gmail.com>
2023-02-11 14:55:15 +0500

    navbar style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,36 @@
 import './navbar.scss';
+import { ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExitOutlined, LanguageOutlined, ListOutlined, NotificationsNoneOutlined, SearchOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        Navbar
+        <div className="wrapper">
+          <div className="search">
+            <input type="text" placeholder="search"/>
+            <SearchOutlined />
+          </div>
+          <div className="items">
+            <div className="item">
+              <LanguageOutlined />
+              English
+            </div>
+            <div className="item">
+              <DarkModeOutlined />
+            </div>
+            <div className="item">
+              <FullscreenExitOutlined />
+            </div>
+            <div className="item">
+              <NotificationsNoneOutlined />
+            </div>
+            <div className="item">
+              <ChatBubbleOutlineOutlined />
+            </div>
+            <div className="item">
+              <ListOutlined />
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
