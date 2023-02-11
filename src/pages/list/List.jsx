d9f169cc7dde5de76cfbb38126cf2384dd94f289
siddiq <msiddiq1400@gmail.com>
2023-02-11 21:39:49 +0500

    add users data table

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,16 @@
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './list.scss';
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className='list'>
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        Datatable
+      </div>
+    </div>
   )
 }
 
