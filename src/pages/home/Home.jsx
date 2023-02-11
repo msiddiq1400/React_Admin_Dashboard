37f447768656329bb2084cb850168d905cb78b39
siddiq <msiddiq1400@gmail.com>
2023-02-11 21:03:08 +0500

    Table Data added for home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import TableData from "../../components/table/TableData";
 import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
@@ -20,6 +21,10 @@ const Home = () => {
             <div className="charts">
               <Featured />
               <Chart />
+            </div>
+            <div className="listContainer">
+              <div className="listTitle">Latest Transactions</div>
+              <TableData />
             </div>
         </div>
     </div>
