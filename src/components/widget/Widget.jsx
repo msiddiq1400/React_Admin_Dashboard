27b30e0d7a8da3365205c046cd9651e6be7c544e
siddiq <msiddiq1400@gmail.com>
2023-02-11 16:27:03 +0500

    added widgets

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,20 +1,44 @@
-import { KeyboardArrowUp, PersonOutline } from '@mui/icons-material';
+import { AccountBalanceWalletOutlined, KeyboardArrowUp, MonetizationOnOutlined, PersonOutline, ShoppingCartOutlined } from '@mui/icons-material';
 import './widget.scss';
 
-const Widget = () => {
+const Widget = ({type}) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 20
+
+  switch(type) {
+    case "user":
+        data = {title: "USERS", isMoney: false, link:"See all users", icon: <PersonOutline className='icon'/>}
+        break;
+    case "order":
+        data = {title: "ORDERS", isMoney: false, link:"View all Orders", icon: <ShoppingCartOutlined className='icon'/>}
+        break;
+    case "earning":
+        data = {title: "EARNINGS", isMoney: true, link:"View net earnings", icon: <MonetizationOnOutlined className='icon'/>}
+        break;
+    case "balance":
+        data = {title: "BALANCE", isMoney: true, link:"See Details", icon: <AccountBalanceWalletOutlined className='icon'/>}
+        break;
+    default:
+        break;
+  }
+
+
   return (
     <div className='widget'>
         <div className="left">
-            <span className="title">USERS</span>
-            <span className="counter">1212</span>
-            <span className="link">See All Users</span>
+            <span className="title">{data.title}</span>
+            <span className="counter">{data.isMoney && `$ ${amount}`}</span>
+            <span className="link">{data.link}</span>
         </div>
         <div className="right">
             <div className="percentage positive">
                 <KeyboardArrowUp />
-                20 %
+                {diff} %
             </div>
-            <PersonOutline  className='icon'/>
+            {data.icon}
         </div>
     </div>
   )
