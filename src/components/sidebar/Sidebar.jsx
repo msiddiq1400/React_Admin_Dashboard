1fbbd42d7742178ff1cc6b95b2ed75c315eb5f49
siddiq <msiddiq1400@gmail.com>
2023-02-11 14:02:15 +0500

    add mui and finish side bar ui

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 const Sidebar = () => {
   return (
@@ -9,13 +21,34 @@ const Sidebar = () => {
         <hr/>
         <div className='center'>
             <ul>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
+                <p className="title">MAIN</p>
+                <li><DashboardIcon className='icon'/><span>Dashboard</span></li>
+                <p className="title">LISTS</p>
+                <li><PersonIcon className='icon'/><span>Users</span></li>
+                <li><StoreIcon className='icon'/><span>Products</span></li>
+                <li><CreditCardIcon className='icon'/><span>Orders</span></li>
+                <li><LocalShippingIcon className='icon'/><span>Delivery</span></li>
+                <p className="title">USEFUL LINKS</p>
+                <li><InsertChartIcon className='icon'/><span>Stats</span></li>
+                <li><NotificationsNoneIcon className='icon'/><span>Notifications</span></li>
+                <p className="title">SERVICE</p>
+                <li><SettingsSystemDaydreamIcon className='icon'/><span>System Health</span></li>
+                <li><PsychologyOutlinedIcon className='icon'/><span>Logs</span></li>
+                <li><SettingsApplicationsOutlinedIcon className='icon'/><span>Settings</span></li>
+                <p className="title">USER</p>
+                <li><AccountCircleOutlinedIcon className='icon'/><span>Profile</span></li>
+                <li><ExitToAppOutlinedIcon className='icon'/><span>Logout</span></li>
             </ul>
         </div>
-        <div className='bottom'>color options</div>
+        <hr/>
+        <div className='bottom'>
+            <div className="colorOption">
+
+            </div>
+            <div className="colorOption">
+                
+            </div>
+        </div>
     </div>
   )
 }
