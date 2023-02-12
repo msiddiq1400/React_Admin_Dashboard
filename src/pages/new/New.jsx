df9a3d1ec53355c5bc83520d44589291d0ee48ac
siddiq <msiddiq1400@gmail.com>
2023-02-12 13:01:43 +0500

    add new product or user

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,14 +1,41 @@
 import './new.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import { DriveFolderUploadOutlined } from '@mui/icons-material';
 
-const New = () => {
+const New = ({title, inputs}) => {
   return (
     <div className='new'>
       <Sidebar />
       <div className="newContainer">
         <Navbar />
-        
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://picsum.photos/600/800" alt="img" />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor='file'>
+                  Image: <DriveFolderUploadOutlined className='icon'/>
+                </label>
+                <input type='file' id='file' hidden />
+              </div>
+              {inputs.map((input) => {
+                return (
+                  <div className="formInput" key={input.id}>
+                    <label>{input.label}</label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                );
+              })}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
