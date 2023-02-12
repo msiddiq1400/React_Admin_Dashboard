9ea8acf10c47493744a2d3d82eab5e9aeae6c253
siddiq <msiddiq1400@gmail.com>
2023-02-12 11:12:36 +0500

    added users page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,50 @@
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Chart from '../../components/chart/Chart';
 import './single.scss';
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://picsum.photos/401/501" alt="itemImage" className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">msiddiq17325@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+923125328555</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Islamabad H13, Service Road NUST</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Pakistan</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart />
+          </div>
+        </div>
+        <div className="bottom">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
