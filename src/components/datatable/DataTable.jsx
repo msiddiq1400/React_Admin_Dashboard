3d80f51adbcf53c55aeda59bacf3a5fd36b9631c
siddiq <msiddiq1400@gmail.com>
2023-02-12 16:04:36 +0500

    delete button added

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -2,16 +2,23 @@ import { Link } from 'react-router-dom';
 import { userColumns, userRows } from '../../datatablesource';
 import './datatable.scss';
 import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
 
 const DataTable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (rowId) => {
+    setData(data.filter(item => item.id !== rowId))
+  }
+
   const actionColums = [
-    {field: "action", headerName: "Action", width: 200, renderCell: () => {
+    {field: "action", headerName: "Action", width: 200, renderCell: (params) => {
         return (
             <div className='cellAction'>
                 <Link to="/users/45" style={{textDecoration: "none"}}>
                     <div className='viewButton'>View</div>
                 </Link>
-                <div className='deleteButton'>Delete</div>
+                <div className='deleteButton' onClick={() => handleDelete(params.row.id)}>Delete</div>
             </div>
         );
     }}
@@ -25,7 +32,7 @@ const DataTable = () => {
         </div>
         <DataGrid
             className='datagrid'
-            rows={userRows}
+            rows={data}
             columns={userColumns.concat(actionColums)}
             pageSize={9}
             rowsPerPageOptions={[9]}
